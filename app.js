bdf22570dde495fa8f6989170ca72d61b8290199
Dave Fisher <dave.j.fish@gmail.com>
2022-06-08 13:16:57 -0700

    can display initial values from state and change character parts

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 // state
 const character = {
-    head: 'bird',
-    middle: 'dress',
+    head: 'duck',
+    middle: 'blue',
     pants: 'white',
     phrases: []
 };
@@ -21,15 +21,21 @@ headSelect.addEventListener('change', () => {
 
 middleSelect.addEventListener('change', () => {
     // *** Complete the middle select change handler
+    character.middle = middleSelect.value;
+    displayDesign();
 });
 
 pantsSelect.addEventListener('change', () => {
     // *** Complete the pants select change handler
+    character.pants = pantsSelect.value;
+    displayDesign();
 });
 
 function displayDesigner() {
     headSelect.value = character.head;
     // *** Assign the initial values of the middle select and pants select!
+    middleSelect.value = character.middle;
+    pantsSelect.value = character.pants;
 }
 
 // AddPhrase
@@ -37,16 +43,21 @@ const addPhraseSection = document.getElementById('add-phrase-section');
 const phraseInput = addPhraseSection.querySelector('input');
 // *** Select the button from the add phrase section
 // const phraseButton = ?;
+const phraseButton = addPhraseSection.querySelector('button');
 
 function handleAddPhrase() {
     // *** add the current phrase value to the character phrases
-
+    character.phrases = phraseInput.value;
     displayPhrases();
     phraseInput.value = '';
     phraseInput.focus();
 }
 
 // *** Add a click handler to the phrase button that calls handleAddPhrase
+phraseButton.addEventListener('click', () => {
+    console.log(character.phrases);
+    handleAddPhrase();
+});
 
 phraseInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
@@ -63,6 +74,9 @@ function displayCharacter() {
     // You can use:
     // 'assets/character/' + <get prop value here> + '-head.png'
     // to format the correct file name
+    headImage.src = 'assets/character/' + character.head + '-head.png';
+    middleImage.src = 'assets/character/' + character.middle + '-middle.png';
+    pantsImage.src = 'assets/character/' + character.pants + '-pants.png';
 }
 
 // Phrases
@@ -73,6 +87,10 @@ function displayPhrases() {
     phraseList.innerHTML = '';
 
     // ** Create an li for each phrase and append to the list
+    const newList = document.createElement('li');
+    if (phraseList) {
+        phraseList.append(newList);
+    }
 }
 
 // page load actions
